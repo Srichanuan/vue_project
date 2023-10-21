3d21fbec78415eeb2fb2fc00902da245cb3a7d50
Srichanuan <143865602+Srichanuan@users.noreply.github.com>
2023-10-21 12:21:09 +0700

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+        <nav class="navbar">
+            <a href="index.html" class="active"> <i class="fas fa-user"></i> <span>profile</span> </a>
+            <a href="about.html"> <i class="fas fa-address-card"></i> <span>about</span> </a>
+            <a href="projects.html"> <i class="fas fa-briefcase"></i> <span>projects</span> </a>
+            <a href="contact.html"> <i class="fas fa-address-book"></i> <span>contact</span> </a>
+        </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Navbar />
+        <section class="home">
+
+            <div class="image">
+                <img src="images/profile.png" alt="">
+            </div>
+
+            <div class="content">
+                <h3>hi, i am Arisa Srichanuan</h3>
+                <span>Belive the Impossible is Possible.</span>
+                <p> I'm Arisa Srichanuan. my nickname is Nun. 20 years old. I were into sports espcially volleyball. I'm easy to get along with, funny</p>
+                <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default {
+    components: {Navbar}
+}
+</script>
+
+<style>
+
+</style>
